e488bf67a3473871a5d48f81e2c4677c4de0a01c
HiddenToad <user@judah-laptop.local>
2022-08-08 21:46:15 -0500

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,111 @@
+pub type Point = (f64, f64);
+use serde::ser::{Serialize, SerializeStruct, Serializer};
+
+pub struct TrainingResult {
+    min_error: f64,
+    intercept: f64,
+    slope: f64,
+}
+
+impl Serialize for TrainingResult{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer
+    {
+        let mut s = serializer.serialize_struct("TrainingResult", 3)?;
+        s.serialize_field("min_error", &self.min_error)?;
+        s.serialize_field("intercept", &self.intercept)?;
+        s.serialize_field("slope", &self.slope)?;
+        s.end()
+        
+    }
+}
+
+impl TrainingResult {
+    pub fn new() -> Self {
+        TrainingResult {
+            min_error: 9999999999999999999999999.,
+            intercept: 0.,
+            slope: 0.,
+        }
+    }
+}
+
+pub struct LinearRegressionModel {
+    epochs: usize,
+    pub graph: Vec<Point>,
+    pub best_result: TrainingResult,
+}
+
+impl LinearRegressionModel {
+    fn round_to_n(number: f64, digits: u32) -> f64 {
+        let exp = 10_u32.pow(digits) as f64;
+        ((number * exp).round()) / exp
+    }
+
+    fn close_enough(number: &mut f64) {
+        if 1. - (*number % 1.) <= 0.0001 {
+            *number = number.round();
+        }
+    }
+
+    pub fn new_uninit() -> Self {
+        LinearRegressionModel {
+            epochs: 100,
+            graph: vec![],
+            best_result: TrainingResult::new(),
+        }
+    }
+
+    pub fn new(input_graph: Vec<Point>) -> Self {
+        LinearRegressionModel {
+            epochs: 100,
+            graph: input_graph,
+            best_result: TrainingResult::new(),
+        }
+    }
+
+    pub fn set_epochs(&mut self, value: usize) {
+        self.epochs = value;
+    }
+
+    pub fn add_points(&mut self, points: &[Point]) {
+        for point in points {
+            self.graph.push(*point);
+        }
+    }
+
+    pub fn train(&mut self) {
+        for x in (1..=100000).rev() {
+            let mut tr = TrainingResult::new();
+            let learn_rate: f64 = x as f64 / 1000000.;
+
+            for i in 0..self.graph.len() * self.epochs {
+                let prediction = tr.intercept + tr.slope * self.graph[i % self.graph.len()].0;
+                let error = prediction - self.graph[i % self.graph.len()].1;
+                tr.intercept -= learn_rate * error;
+                tr.slope -= (learn_rate * error) * self.graph[i % self.graph.len()].0;
+                if error.abs() < tr.min_error {
+                    tr.min_error = error.abs();
+                }
+            }
+            if tr.min_error.abs() < self.best_result.min_error {
+                tr.min_error = tr.min_error.abs();
+                Self::close_enough(&mut tr.slope);
+                Self::close_enough(&mut tr.intercept);
+                self.best_result = tr;
+            }
+        }
+        println!(
+            "best result:\nslope: {}\nintercept: {}\nerror: {}",
+            self.best_result.slope, self.best_result.intercept, self.best_result.min_error
+        );
+    }
+
+    pub fn predict(self, input: f64) -> f64 {
+        Self::round_to_n(
+            self.best_result.intercept + self.best_result.slope * input,
+            2,
+        )
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+#![allow(non_upper_case_globals)]
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder, HttpResponseBuilder, http::StatusCode, http::header::ContentType};
+use linear_regression::{LinearRegressionModel, Point, TrainingResult};
+use std::{fs::read_to_string, num::ParseFloatError};
+
+fn do_train(req_body: String) -> Result<TrainingResult, ParseFloatError>{
+    let mut model = LinearRegressionModel::new_uninit();
+
+    let req_body = req_body
+        .replace("data=", "")
+        .replace(" ", "")
+        .replace("(", "")
+        .replace(")", "");
+    
+        for line in req_body.lines() {
+            let line: Vec<&str> = line.trim().split(",").collect();
+            model.add_points(&[(line[0].parse()?, line[1].parse()?)]);
+        }
+
+        model.train();
+
+        Ok(model.best_result)
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    //todo: link to html file
+    HttpResponse::Ok().body(read_to_string("src/index.html").unwrap())
+}
+
+#[post("/train")]
+async fn train(req_body: String) -> impl Responder {
+    println!("{}", req_body);
+    let result = do_train(req_body);
+
+    match result{
+        Ok(value) => {
+            return HttpResponseBuilder::new(StatusCode::OK)
+                .json(value);
+        }
+        Err(_) => {
+            return HttpResponseBuilder::new(StatusCode::OK)
+                .content_type(ContentType::plaintext())
+                .body("INVALIDINPUT");
+        }
+    }
+
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(train))
+        .bind(("0.0.0.0", std::env::var("PORT").unwrap().parse().unwrap()))?
+        .run()
+        .await
+}

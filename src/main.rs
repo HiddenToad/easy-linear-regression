136b439d2bd752dac097e539c149b0c9dcc701c9
HiddenToad <user@judah-laptop.local>
2022-08-09 12:25:44 -0500

    mebbe

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn do_train(req_body: String) -> Result<TrainingResult, ParseFloatError>{
             model.add_points(&[(line[0].parse()?, line[1].parse()?)]);
         }
 
-        model.set_epochs(model.graph.len() * 15000);
+        model.set_epochs(model.graph.len() * 20000);
 
         model.train();
 

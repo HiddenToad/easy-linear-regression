68f3074b2d418b69c1cdbb4b0f83b0b2da882e2b
HiddenToad <user@judah-laptop.local>
2022-08-16 09:17:23 -0500

    increased iterations

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn do_train(req_body: String) -> Result<TrainingResult, ParseFloatError>{
             model.add_points(&[(line[0].parse()?, line[1].parse()?)]);
         }
 
-        model.set_epochs(20_000_000);
+        model.set_epochs(50_000_000);
 
         model.train();
 
